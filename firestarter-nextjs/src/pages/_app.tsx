6d201f9d9e606e54ca45748f17b0f25b5752f0bf
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-04 10:22:56 +0000

    Use tw-elements, with nav example usage

--- firestarter-nextjs/src/pages/_app.tsx
+++ firestarter-nextjs/src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../styles/index.css'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  // Needed to load tw-elements on client side only (See: https://stackoverflow.com/questions/71875270/how-to-install-the-tailwind-elements-in-nextjs)
+  useEffect(() => {
+    const use = async () => {
+      (await import('tw-elements')).default;
+    };
+    use();
+  }, []);
+
   return <Component {...pageProps} />
 }
