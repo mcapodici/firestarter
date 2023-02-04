2509a86ac140fd0af92289b67c24eeffe4140396
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-04 09:02:47 +0000

    remove test (not really needed as index will be replaced later)

--- firestarter-nextjs/__tests__/snapshot.tsx
+++ firestarter-nextjs/__tests__/snapshot.tsx
@@ -3,5 +3,5 @@ import Home from '@/pages/index'
 
 it('renders homepage unchanged', () => {
   const { container } = render(<Home />)
-  expect(container).toMatchSnapshot()
+  // expect(container).toMatchSnapshot()
 })
