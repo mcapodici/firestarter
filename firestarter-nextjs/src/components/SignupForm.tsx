456d2c22430916af533c2891e30aa284dce6a799
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-08 22:42:32 +0000

    add signup form page

---
+++ firestarter-nextjs/src/components/SignupForm.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+interface Props {
+    imgsrc: string;
+    heading: React.ReactNode;
+}
+
+// Based on https://tailwind-elements.com/docs/standard/designblocks/hero-sections/
+
+export default function SignupForm({ imgsrc, heading }: Props) {
+    return <div>
+    </div>;
+}
