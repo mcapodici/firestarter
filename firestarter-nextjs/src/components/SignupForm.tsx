e542064c727e2144869347531bdb56613f80251b
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-08 22:45:24 +0000

    remove props from SignupForm

--- firestarter-nextjs/src/components/SignupForm.tsx
+++ firestarter-nextjs/src/components/SignupForm.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image'
-
 interface Props {
-    imgsrc: string;
-    heading: React.ReactNode;
 }
 
-// Based on https://tailwind-elements.com/docs/standard/designblocks/hero-sections/
-
-export default function SignupForm({ imgsrc, heading }: Props) {
+export default function SignupForm() {
     return <div>
     </div>;
 }
