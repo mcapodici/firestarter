94dceee73675b2e3bae6d0cc17e953e994d1db49
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-18 21:41:49 +0000

    refactored signup test so it is easy for each test to change the mock

--- firestarter-nextjs/__tests__/backend/signup.test.tsx
+++ firestarter-nextjs/__tests__/backend/signup.test.tsx
@@ -2,18 +2,21 @@ import signup from '@/backend/Signup';
 import * as dep from 'firebase/auth';
 import { UserCredential } from 'firebase/auth';
 
-let mockCredentials = {} as any as UserCredential;
+const createUserWithEmailAndPassword = jest.fn();
 jest.mock<Partial<typeof dep>>('firebase/auth',
     () => ({
-        createUserWithEmailAndPassword: () => Promise.resolve(mockCredentials)
+        createUserWithEmailAndPassword: (...args) => createUserWithEmailAndPassword(...args)
     }));
-    
+
+   
 describe('Signup Function', () => {
     it('Works with successful signup', async () => {
-        mockCredentials = { user: { uid: "123" } } as any as UserCredential;
+        const mockCredentials = { user: { uid: "123" } } as any as UserCredential;
+        createUserWithEmailAndPassword.mockReturnValue(mockCredentials);
         const result = await signup('ben@example.com', 'fred1234!');
         if (result.result !== 'success')
             fail("Expect success");
         expect(result.uid).toBe('123');
+        expect(createUserWithEmailAndPassword).toBeCalledWith(undefined, 'ben@example.com', 'fred1234!');
     });
 });
