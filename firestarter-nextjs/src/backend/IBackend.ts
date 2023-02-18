00dff4bc46f74c323c1fad114cef78c761be4fad
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-18 23:26:09 +0000

    check all of the signup error codes

--- firestarter-nextjs/src/backend/IBackend.ts
+++ firestarter-nextjs/src/backend/IBackend.ts
@@ -1,7 +1,16 @@
 export type SignupSuccess = { result: 'success', uid: string };
 export type SignupFail = { result: 'fail', message: string };
 export type SignupFailWeakPassword = { result: 'weak-password' };
-export type SignupResult = SignupSuccess | SignupFail | SignupFailWeakPassword;
+export type SignupFailAccountsNotEnabledOnBackend = { result: 'accounts-not-enabled' };
+export type SignupFailEmailIsInUse = { result: 'email-in-use' };
+export type SignupFailInvalidEmail = { result: 'invalid-email' };
+export type SignupResult =
+    SignupSuccess |
+    SignupFail |
+    SignupFailWeakPassword |
+    SignupFailAccountsNotEnabledOnBackend |
+    SignupFailEmailIsInUse |
+    SignupFailInvalidEmail;
 
 export interface IBackend {
     signup(email: string, password: string): Promise<SignupResult>;
