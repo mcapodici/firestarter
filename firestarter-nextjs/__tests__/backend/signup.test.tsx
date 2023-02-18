41c25e61a6300fb585e1930ccd9b3adba68d9a8f
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-18 21:47:00 +0000

    added weak password result test

--- firestarter-nextjs/__tests__/backend/signup.test.tsx
+++ firestarter-nextjs/__tests__/backend/signup.test.tsx
@@ -1,4 +1,5 @@
 import signup from '@/backend/Signup';
+import { FirebaseError } from '@firebase/util';
 import * as dep from 'firebase/auth';
 import { UserCredential } from 'firebase/auth';
 
@@ -10,13 +11,24 @@ jest.mock<Partial<typeof dep>>('firebase/auth',
 
    
 describe('Signup Function', () => {
-    it('Works with successful signup', async () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+
+    it('Works with successful signup', async () => {        
         const mockCredentials = { user: { uid: "123" } } as any as UserCredential;
-        createUserWithEmailAndPassword.mockReturnValue(mockCredentials);
+        createUserWithEmailAndPassword.mockResolvedValue(mockCredentials);
         const result = await signup('ben@example.com', 'fred1234!');
         if (result.result !== 'success')
             fail("Expect success");
         expect(result.uid).toBe('123');
         expect(createUserWithEmailAndPassword).toBeCalledWith(undefined, 'ben@example.com', 'fred1234!');
+    });
+
+    it('Handles a weak password result from Firebase', async () => {        
+        createUserWithEmailAndPassword.mockRejectedValue(new FirebaseError('auth/weak-password', 'Weak PW'));
+        const result = await signup('ben@example.com', 'fred');
+        expect(result.result).toBe('weak-password');
+        expect(createUserWithEmailAndPassword).toBeCalledWith(undefined, 'ben@example.com', 'fred');
     });
 });
