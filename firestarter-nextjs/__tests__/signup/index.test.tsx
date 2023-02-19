0b0b4cd612318d68963ed6b4b4ee62f9511a85e8
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-19 02:01:32 +0000

    unit test first name and last name validation

--- firestarter-nextjs/__tests__/signup/index.test.tsx
+++ firestarter-nextjs/__tests__/signup/index.test.tsx
@@ -11,7 +11,10 @@ describe('Signup', () => {
     backend: { signup: jest.fn(async (u: string, p: string) => ({ result: 'fail' } as SignupResult)) }
   };
 
+  let user: UserEvent;
+
   beforeEach(() => {
+    user = userEvent.setup();
     render(
       <Context.Provider value={mockContext}>
         <Signup />
@@ -23,11 +26,22 @@ describe('Signup', () => {
     jest.clearAllMocks();
   });
 
-  async function fillInAllFieldsValid(user: UserEvent) {
+  async function fillInAllFieldsValid() {
     await user.type(screen.getByPlaceholderText('First name'), 'Ben');
     await user.type(screen.getByPlaceholderText('Last name'), 'Neb');
     await user.type(screen.getByPlaceholderText('Email address'), 'me@them.com');
     await user.type(screen.getByPlaceholderText('Password'), 'password123');
+  }
+
+  function expectNoSignupCall() {
+    expect(mockContext.backend.signup).not.toBeCalled();
+  }
+
+  async function submitFormAndCheckAlertText(expectedAlert: string) {
+    await user.click(screen.getByText('Sign up'));
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(1);
+    expect(alerts[0]).toHaveTextContent(expectedAlert);
   }
 
   it('correct form elements shown', () => {
@@ -41,78 +55,76 @@ describe('Signup', () => {
   });
 
   it('sends the submitted data to the signup service', async () => {
-    const user = userEvent.setup();
-    await fillInAllFieldsValid(user);
+    await fillInAllFieldsValid();
     await user.click(screen.getByText('Sign up'));
     expect(mockContext.backend.signup).toBeCalledWith("me@them.com", "password123", { firstName: 'Ben', lastName: 'Neb' });
   });
 
+  it('validates the first name exists', async () => {
+    await fillInAllFieldsValid();
+    await user.clear(screen.getByPlaceholderText('First name'));
+    await submitFormAndCheckAlertText("First name is required");
+    expectNoSignupCall();
+  });
+
+  it('validates the last name exists', async () => {
+    await fillInAllFieldsValid();
+    await user.clear(screen.getByPlaceholderText('Last name'));
+    await submitFormAndCheckAlertText("Last name is required");
+    expectNoSignupCall();
+  });
+
+
   it('validates the email address exists', async () => {
-    const user = userEvent.setup();
-    await fillInAllFieldsValid(user);
+    await fillInAllFieldsValid();
     await user.clear(screen.getByPlaceholderText('Email address'));
-    await user.click(screen.getByText('Sign up'));
-    expect(mockContext.backend.signup).not.toBeCalled();
-    const alerts = await screen.findAllByRole("alert");
-    expect(alerts).toHaveLength(1);
-    expect(alerts[0]).toHaveTextContent("Email address is required");
+    await submitFormAndCheckAlertText("Email address is required");
+    expectNoSignupCall();
   });
 
   it('validates the password exists', async () => {
-    const user = userEvent.setup();
-    await fillInAllFieldsValid(user);
+    await fillInAllFieldsValid();
     await user.clear(screen.getByPlaceholderText('Password'));
-    await user.click(screen.getByText('Sign up'));
-    expect(mockContext.backend.signup).not.toBeCalled();
-    const alerts = await screen.findAllByRole("alert");
-    expect(alerts).toHaveLength(1);
-    expect(alerts[0]).toHaveTextContent("Password is required");
+    await submitFormAndCheckAlertText("Password is required");
+    expectNoSignupCall();
   });
 
   it('validates the email format', async () => {
-    const user = userEvent.setup();
-    await fillInAllFieldsValid(user);
+    await fillInAllFieldsValid();
     await user.clear(screen.getByPlaceholderText('Email address'));
     await user.type(screen.getByPlaceholderText('Email address'), 'methem');
-    await user.click(screen.getByText('Sign up'));
-    expect(mockContext.backend.signup).not.toBeCalled();
-    const alerts = await screen.findAllByRole("alert");
-    expect(alerts).toHaveLength(1);
-    expect(alerts[0]).toHaveTextContent("Email address is invalid");
+    await submitFormAndCheckAlertText("Email address is invalid");
+    expectNoSignupCall();
   });
 
   describe('handles firebase error return code', () => {
-    async function submitFormAndCheckAlertText(expectedAlert: string) {
-      const user = userEvent.setup();
-      await fillInAllFieldsValid(user);
-      await user.click(screen.getByText('Sign up'));
-      const alerts = await screen.findAllByRole("alert");
-      expect(alerts).toHaveLength(1);
-      expect(alerts[0]).toHaveTextContent(expectedAlert);
-    }
-
     it('invalid-email', async () => {
       mockContext.backend.signup.mockResolvedValue({ result: 'invalid-email' });
+      await fillInAllFieldsValid();
       await submitFormAndCheckAlertText("Email address is invalid");
     });
 
     it('accounts-not-enabled', async () => {
       mockContext.backend.signup.mockResolvedValue({ result: 'accounts-not-enabled' });
+      await fillInAllFieldsValid();
       await submitFormAndCheckAlertText("Sorry there was a server problem while signing up, please try again later.");
     });
 
     it('email-in-use', async () => {
       mockContext.backend.signup.mockResolvedValue({ result: 'email-in-use' });
+      await fillInAllFieldsValid();
       await submitFormAndCheckAlertText("An account with this email already exists. Please pick another email, or try signing in.");
     });
 
     it('fail', async () => {
       mockContext.backend.signup.mockResolvedValue({ result: 'fail', message: 'Error 500' });
+      await fillInAllFieldsValid();
       await submitFormAndCheckAlertText("Sorry there was a server problem while signing up, please try again later.");
     });
 
     it('weak-password', async () => {
       mockContext.backend.signup.mockResolvedValue({ result: 'weak-password' });
+      await fillInAllFieldsValid();
       await submitFormAndCheckAlertText("Password doesn't meet the requirements. Password should have at least 6 characters.");
     });
   });
