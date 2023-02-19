248eecb8bf459b6e5c605e1b5778376258d0561d
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-19 01:28:59 +0000

    avoid verbose warnings on test fails

--- firestarter-nextjs/jest.setup.js
+++ firestarter-nextjs/jest.setup.js
@@ -4,3 +4,14 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+
+import {configure} from '@testing-library/dom'
+
+configure({
+    getElementError: (message, container) => {
+      const error = new Error(message);
+      error.name = 'TestingLibraryElementError';
+      error.stack = null;
+      return error;
+    },
+  });
