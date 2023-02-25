821c3b9825af2f924ef4c7a2c88067a447ec29db
Martin Capodici <mcapodici@users.noreply.github.com>
2023-02-25 06:22:46 +0000

    remove need for tw-elements dependency

--- firestarter-nextjs/src/components/Nav.tsx
+++ firestarter-nextjs/src/components/Nav.tsx
@@ -1,21 +1,31 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Nav() {
-  return <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+
+  const [expandedNav, setExpandedNav] = useState(false);
+
+  return <nav className="shadow-md py-2 bg-white relative flex items-center w-full justify-between">
     <div className="px-6 w-full flex flex-wrap items-center justify-between">
       <div className="flex items-center">
-        <button className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentY" aria-controls="navbarSupportedContentY" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          onClick={() => { setExpandedNav(x => !x) }}
+          className="border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
+          type="button"
+          aria-controls="navigationItems"
+          aria-expanded={expandedNav}
+          aria-label="Toggle navigation">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
           </svg>
         </button>
-        <a className="navbar-brand text-blue-600 mr-2" href="#!">
+        <a className="text-blue-600 mr-2" href="/">
           <Image src="/logo.png" alt="Logo" height={24} width={24} />
         </a>
       </div>
-      <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
-        <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
+      <div className={`${expandedNav ? '' : 'hidden'} lg:block grow items-center basis-full lg:basis-0`}  id="navigationItems">
+        <ul className="mr-auto lg:flex lg:flex-row">
           <li className="nav-item">
             <Link href="/" className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out">Dashboard</Link>
           </li>
@@ -27,7 +37,7 @@ export default function Nav() {
           </li>
         </ul>
       </div>
-      <div className="flex items-center lg:ml-auto">
+      <div className={`${expandedNav ? '' : 'hidden'} lg:block flex items-center lg:ml-auto`}>
         <button type="button" className="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out">Login</button>
 
         <Link href="/signup" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sign up for free</Link>
