64599aa11deda014e017f6e63cb5210b08be27ba
Martin Capodici <martin.capodici@gmail.com>
2023-03-03 12:09:32 +1100

    Refactor backend to remove unneeded types

--- nextjs/src/backend/IBackend.ts
+++ nextjs/src/backend/IBackend.ts
@@ -1,22 +1,15 @@
-export type SignupSuccess = { result: 'success', uid: string };
+export type SignupResult =
+{ result: 'success', uid: string } |
 
 /**
  * Represents success in creating user, but a failure to store user data
  */
-export type SignupPartialSuccess = { result: 'partial-success', uid: string };
-export type SignupFail = { result: 'fail', message: string };
-export type SignupFailWeakPassword = { result: 'weak-password' };
-export type SignupFailAccountsNotEnabledOnBackend = { result: 'accounts-not-enabled' };
-export type SignupFailEmailIsInUse = { result: 'email-in-use' };
-export type SignupFailInvalidEmail = { result: 'invalid-email' };
-export type SignupResult =
-    SignupSuccess |
-    SignupPartialSuccess |
-    SignupFail |
-    SignupFailWeakPassword |
-    SignupFailAccountsNotEnabledOnBackend |
-    SignupFailEmailIsInUse |
-    SignupFailInvalidEmail;
+{ result: 'partial-success', uid: string } |
+{ result: 'fail', message: string } |
+{ result: 'weak-password' } |
+{ result: 'accounts-not-enabled' } |
+{ result: 'email-in-use' } |
+{ result: 'invalid-email' };
 
 export type LoginResult =
     { result: 'success', uid: string } |
