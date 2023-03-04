87e436cbbde35343485a19d9e49d010ad4d2e872
Martin Capodici <martin.capodici@gmail.com>
2023-03-04 12:09:33 +1100

    put code in to fetch the todos

--- nextjs/src/pages/todos/index.tsx
+++ nextjs/src/pages/todos/index.tsx
@@ -1,6 +1,26 @@
+import { Todo } from '@/backend/IBackend';
 import Layout from '@/components/Layout'
+import { Context } from '@/context/Context'
+import { useContext, useEffect, useState } from 'react'
 
 export default function Todos() {
+
+  const { user, backend } = useContext(Context);
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    console.log('user', user);
+    if (user) {
+      backend.getTodos(user.uid).then((todos) => {
+        console.log(todos);
+        if (todos.result === 'success') {
+          setTodos(todos.items);
+        }
+      })
+    }
+
+  }, [user])
 
   return (
     <>
@@ -9,28 +29,23 @@ export default function Todos() {
           <h1 className="text-5xl font-bold pb-2 mt-0 mb-6 bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-black">Todos</h1>
         </div>
         <div className="m-auto max-w-5xl p-2">
-          <table className="min-w-full text-left text-sm font-light">
-            <thead className="border-b font-medium dark:border-neutral-500">
-              <tr>
-                <th scope="col" className="px-6 py-4 w-2">#</th>
-                <th scope="col" className="px-6 py-4">What</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border-b dark:border-neutral-500">
-                <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                <td className="whitespace-nowrap px-6 py-4">Eat Cake</td>
-              </tr>
-              <tr className="border-b dark:border-neutral-500">
-                <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                <td className="whitespace-nowrap px-6 py-4">Run Mile</td>
-              </tr>
-              <tr className="border-b dark:border-neutral-500">
-                <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
-                <td className="whitespace-nowrap px-6 py-4">Drink Water</td>
-              </tr>
-            </tbody>
-          </table>
+          {todos.length > 0 &&
+            <table className="min-w-full text-left text-sm font-light">
+              <thead className="border-b font-medium dark:border-neutral-500">
+                <tr>
+                  <th scope="col" className="px-6 py-4 w-2">#</th>
+                  <th scope="col" className="px-6 py-4">What</th>
+                </tr>
+              </thead>
+              <tbody>
+                {todos.map((todo, index) =>
+                  <tr key={index} className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">{index + 1}</td>
+                    <td className={`whitespace-nowrap px-6 py-4 ` + (todo.done ? 'line-through' : '')}>{todo.title}</td>
+                  </tr>)}
+              </tbody>
+            </table>
+          }
           <div className="mt-2 text-right">
             <button type="submit" className="button blue w-full md:w-fit">Add Todo</button>
           </div>
