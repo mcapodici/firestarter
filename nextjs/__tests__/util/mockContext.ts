4838a2beca767b1f12448ac0e6ca945527c1a59f
Martin Capodici <martin.capodici@gmail.com>
2023-03-08 19:54:51 +1100

    add mocks to test

--- nextjs/__tests__/util/mockContext.ts
+++ nextjs/__tests__/util/mockContext.ts
@@ -1,5 +1,30 @@
-export const makeMockContext = () => ({
-    backend: { signup: jest.fn(), login: jest.fn(), resetPassword: jest.fn() },
+import { IBackend } from "@/backend/IBackend";
+import { ContextInterface } from "@/context/Context";
+
+export const makeMockContext = () => {
+
+  const backendMocks = {
+    signup: jest.fn(),
+    login: jest.fn(),
+    resetPassword: jest.fn(),
+    addTodo: jest.fn(),
+    setTodo: jest.fn(),
+    getTodos: jest.fn(),
+    deleteTodo: jest.fn(),
+  };
+
+  // Type check
+  const backend = backendMocks as IBackend;
+
+  const mocks = {
+    backend: backendMocks,
     toasts: [],
-    addToast: jest.fn()
-});
+    addToast: jest.fn(),
+    user: undefined,
+  };
+
+  // Type check
+  const context = mocks as ContextInterface;
+
+  return mocks;
+};
