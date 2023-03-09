6ff42293c0a1be0df7c9c84f45ceadd4387828fe
Martin Capodici <martin.capodici@gmail.com>
2023-03-09 20:47:56 +1100

    add todos to nav

--- nextjs/src/components/Nav.tsx
+++ nextjs/src/components/Nav.tsx
@@ -27,7 +27,7 @@ export default function Nav() {
       <div className={`${expandedNav ? '' : 'hidden'} lg:block grow items-center basis-full lg:basis-0`}  id="navigationItems">
         <ul className="mr-auto lg:flex lg:flex-row">
           <li className="nav-item">
-            <Link href="/" className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out">Dashboard</Link>
+            <Link href="/todos" className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out">Todos</Link>
           </li>
           <li className="nav-item">
             <Link href="/" className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out">Team</Link>
