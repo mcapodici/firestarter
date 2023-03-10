01497f9c33ea594e704cb6bec1101ba1495c6165
Martin Capodici <martin.capodici@gmail.com>
2023-03-10 20:38:01 +1100

    logout button (non functional)

--- nextjs/src/components/Nav.tsx
+++ nextjs/src/components/Nav.tsx
@@ -88,6 +88,16 @@ export default function Nav() {
             </Link>
           )}
 
+          {user && (
+            <button
+              onClick={() => {}}
+              type="button"
+              className="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+            >
+              Logout
+            </button>
+          )}
+
           <Link
             href="/signup"
             className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
