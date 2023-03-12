4fd9f652a18e29262546df4d127a64c27d6bb5e8
Martin Capodici <martin.capodici@gmail.com>
2023-03-12 20:13:48 +1100

    added ui tests

---
+++ nextjs/__tests__/logout/index.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+import { Context } from "@/context/Context";
+import userEvent from "@testing-library/user-event";
+import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
+import { makeMockContext } from "__tests__/util/mockContext";
+import Index from "@/pages/index";
+import { User } from "firebase/auth";
+
+jest.mock("next/router", () => require("next-router-mock"));
+
+describe("Logout", () => {
+  const mockContext = makeMockContext();
+
+  let user: UserEvent;
+
+  function doRender(loggedIn: boolean) {
+    user = userEvent.setup();
+    mockContext.user = loggedIn ? ({ uid: "123" } as User) : undefined;
+    render(
+      <Context.Provider value={mockContext}>
+        <Index />
+      </Context.Provider>,
+      { wrapper: MemoryRouterProvider }
+    );
+  }
+
+  it("if logged in, can log out", async () => {
+    doRender(true);
+    mockContext.backend.logout.mockResolvedValue(undefined);
+    expect(screen.getByText(/log out/i)).toBeInTheDocument();
+    await user.click(screen.getByText(/log out/i));
+    expect(mockContext.backend.logout).toBeCalled();
+    // Note: We can't assert that the log out button has disappeared, as this is requires a change to authentication state
+    // (picked up by useAuthState) which is used by _app.tsx, which is not under test here.
+  });
+
+  it("if not logged in, can 'tlog out", async () => {
+    doRender(false);
+    mockContext.backend.logout.mockResolvedValue(undefined);
+    expect(screen.queryByText(/log out/i)).toBeNull();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
--- nextjs/src/components/Nav.tsx
+++ nextjs/src/components/Nav.tsx
@@ -91,15 +91,15 @@ export default function Nav() {
 
           {user && (
             <button
-              onClick={async () => {
-                backend.logout();
+              onClick={async () => {                
+                await backend.logout();
                 await router.push("/login");
                 addToast("You are now logged out.", "success");
               }}
               type="button"
               className="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
             >
-              Logout
+              Log out
             </button>
           )}
 
